93f98b30f3831a742522150e5b1b35055f6934fc
naufalnc <153064625+Nuafalnc@users.noreply.github.com>
2024-05-01 23:50:39 +0700

    Add files via upload
    web profile Naufal Nurcahyo

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//toggle cls active
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik dimana saja untuk menghilangkan navbar
+
+const hamburger = document.querySelector("#humburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
